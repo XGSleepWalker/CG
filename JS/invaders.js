64858f53c2722f964d09aa20113b8e7ed3eefec5
Frederico Santos <ico2555@hotmail.com>
2016-10-06 12:33:55 +0100

    Initial Squid Unfinished structure

--- JS/invaders.js
+++ JS/invaders.js
@@ -68,7 +68,56 @@ function createShip(x, y, z) {
       ship.position.y = y;
       ship.position.z = z;
 }
+function addSquidHead1(obj, x, y, z, material){
+	var geometry = new THREE.CubeGeometry(10,5,0);
+	var mesh = new THREE.Mesh(geometry, material);
+	mesh.position.set(x,y,z);
 
+	obj.add(mesh);
+
+}
+function addSquidHead2(obj, x, y, z, material){
+	var geometry = new THREE.CubeGeometry(20,5,0);
+	var mesh = new THREE.Mesh(geometry, material);
+	mesh.position.set(x,y,z);
+	obj.add(mesh);
+}
+function addSquidHead3(obj, x, y, z, material){
+	var geometry = new THREE.CubeGeometry(30, 5, 0);
+	var mesh = new THREE.Mesh(geometry, material);
+	mesh.position.set(x,y,z);
+	obj.add(mesh);
+}
+function addSquidEyesLeftSide(obj, x,y,z, material){
+	var geometry = new THREE.CubeGeometry(10, 5, 0);
+	var mesh = new THREE.Mesh(geometry, material);
+	mesh.position.set(x,y,z);
+	obj.add(mesh);
+}
+function addSquidEyesCenterSide(obj, x,y,z, material){
+	var geometry = new THREE.CubeGeometry(15, 5, 0);
+	var mesh = new THREE.Mesh(geometry, material);
+	mesh.position.set(x,y,z);
+	obj.add(mesh);
+}
+function addSquidEyesRightSide(obj, x,y,z, material){
+	var geometry = new THREE.CubeGeometry(10,5,0);
+	var mesh = new THREE.Mesh(geometry, material);
+	mesh.position.set(x,y,z);
+	obj.add(mesh);
+}
+function createSquid(x, y, z){
+	'use strict';
+	var alien = new THREE.Object3D();
+	var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+
+
+	scene.add(alien);
+	alien.position.x = x;
+	alien.position.y = y;
+	alien.position.z = z;
+
+}
 /*function createPlayField(x, y, z) {
 	'use strict';
 	var material = new THREE.NoColors();
@@ -90,7 +139,7 @@ function createCamera() {
       camera.lookAt(scene.position);
 }
 
-function createScene() {
+function createScene() 	{
       'use strict';
       scene = new THREE.Scene();
       //scene.add(new THREE.AxisHelper(10));
