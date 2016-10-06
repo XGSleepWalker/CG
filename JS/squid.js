29ebafe5b32d8c8f073f9431b7c5fd409157560a
Frederico Santos <ico2555@hotmail.com>
2016-10-06 15:27:56 +0100

    Squid Alien-shaped finished

--- JS/squid.js
+++ JS/squid.js
@@ -1,37 +1,5 @@
-function addSquidHead1(obj, x, y, z, material){
-	var geometry = new THREE.CubeGeometry(10,5,0);
-	var mesh = new THREE.Mesh(geometry, material);
-	mesh.position.set(x,y,z);
-
-	obj.add(mesh);
-
-}
-function addSquidHead2(obj, x, y, z, material){
-	var geometry = new THREE.CubeGeometry(20,5,0);
-	var mesh = new THREE.Mesh(geometry, material);
-	mesh.position.set(x,y,z);
-	obj.add(mesh);
-}
-function addSquidHead3(obj, x, y, z, material){
-	var geometry = new THREE.CubeGeometry(30, 5, 0);
-	var mesh = new THREE.Mesh(geometry, material);
-	mesh.position.set(x,y,z);
-	obj.add(mesh);
-}
-function addSquidEyesLeftSide(obj, x,y,z, material){
-	var geometry = new THREE.CubeGeometry(10, 5, 0);
-	var mesh = new THREE.Mesh(geometry, material);
-	mesh.position.set(x,y,z);
-	obj.add(mesh);
-}
-function addSquidEyesCenterSide(obj, x,y,z, material){
-	var geometry = new THREE.CubeGeometry(15, 5, 0);
-	var mesh = new THREE.Mesh(geometry, material);
-	mesh.position.set(x,y,z);
-	obj.add(mesh);
-}
-function addSquidEyesRightSide(obj, x,y,z, material){
-	var geometry = new THREE.CubeGeometry(10,5,0);
+function addSquidParts(obj, x,y,z, material, sizex, sizey, sizez){
+	var geometry = new THREE.CubeGeometry(sizex, sizey, sizez);
 	var mesh = new THREE.Mesh(geometry, material);
 	mesh.position.set(x,y,z);
 	obj.add(mesh);
@@ -40,7 +8,22 @@ function createSquid(x, y, z){
 	'use strict';
 	alien = new THREE.Object3D();
 	var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-
+	addSquidParts(alien, 14,28,0, material, 2*alienscale,1*alienscale,0); // Head part 1
+	addSquidParts(alien, 14,24,0, material, 4*alienscale,1*alienscale,0); // Head part 2
+	addSquidParts(alien, 14,20,0, material, 6*alienscale,1*alienscale,0); // Head part 3
+	addSquidParts(alien, 2,16,0, material, 2*alienscale,1*alienscale,0); // Eyes, left side
+	addSquidParts(alien, 14,16,0, material, 3*alienscale,1*alienscale,0); // Eyes, center side
+	addSquidParts(alien, 26,16,0, material, 2*alienscale,1*alienscale,0); // Eyes, right side
+	addSquidParts(alien, 14,12,0, material, 8*alienscale,1*alienscale,0); // Body base
+	addSquidParts(alien, 8,8,0, material, 1*alienscale,1*alienscale,0); //  Legs Layer 1, left side
+	addSquidParts(alien, 20,8,0, material, 1*alienscale,1*alienscale,0); //  Legs Layer 1, right side
+	addSquidParts(alien, 4,4,0, material, 1*alienscale,1*alienscale,0); //  Legs Layer 2, left side
+	addSquidParts(alien, 14,4,0, material, 2*alienscale,1*alienscale,0); // Legs Layer 2, center side
+	addSquidParts(alien, 24,4,0, material, 1*alienscale,1*alienscale,0); //  Legs Layer 2, right side
+	addSquidParts(alien, 0,0,0, material, 1*alienscale,1*alienscale,0); //  Legs Layer 3, Left-most side
+	addSquidParts(alien, 8,0,0, material, 1*alienscale,1*alienscale,0); //  Legs Layer 3, left-center side
+	addSquidParts(alien, 20,0,0, material, 1*alienscale,1*alienscale,0); //  Legs Layer 3, right-center side
+	addSquidParts(alien, 28,0,0, material, 1*alienscale,1*alienscale,0); //  Legs Layer 3, right-most side
 
 	scene.add(alien);
 	alien.position.x = x;
