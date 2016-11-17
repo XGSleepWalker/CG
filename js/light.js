af8cd19d40be74e644f23a72ff81f6e9e7710998
davtrig <davtrig@users.noreply.github.com>
2016-11-17 14:46:33 +0000

    pointLight.intensity = 0

--- js/light.js
+++ js/light.js
@@ -22,7 +22,7 @@ function createPointLight() {
 	var k = pointLightsNmr;
 	for(i = 0; i < pointLightsLines; i++) {
 		for(j = 0; j < pointLightsPerLine; j++) {
-			pointLights[k] = new THREE.PointLight( 0xffffff, 6, 250 );
+			pointLights[k] = new THREE.PointLight( 0xffffff, 0, 250 );
 			pointLights[k].position.set(-400 + (pointLightsGap*j), 300 - (pointLightsGap*i*1.2), 20 );
 			scene.add( pointLights[k] );
 			console.log("point light created: " + k);
